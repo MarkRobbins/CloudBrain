ff2af021391128efe3622646adda213b39625c59
Mark Robbins <mark@mrobbinsassoc.com>
2012-08-07 10:02:24 -0400

    Members

--- downcopy.bat
+++
@@ -1,2 +0,0 @@
-copy "C:\Documents and Settings\Mark\Application Data\Mozilla\Firefox\Profiles\wwguketg.default\gm_scripts\CloudBrain\CloudBrain.user.js" "C:\!mark\_dev\CloudBrain_vpj\CloudBrain.user.js" /y
-copy "C:\Documents and Settings\Mark\Application Data\Mozilla\Firefox\Profiles\wwguketg.default\gm_scripts\for_iframing\for_iframing.user.js"  "C:\!mark\_dev\CloudBrain_vpj\for_iframing.user.js" /y
